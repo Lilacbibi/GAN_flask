88d9b043e4cbc9f4a499ed31612b6baccad84832
Lilacbibi <133239134+Lilacbibi@users.noreply.github.com>
2023-12-06 13:59:11 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, Response
+from flask import Flask, render_template, request, redirect, url_for, flash, Response, session
+
 import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt
@@ -6,6 +7,7 @@ from tensorflow import keras
 from io import BytesIO
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import base64
+import gc
 
 app = Flask(__name__)
 app.secret_key = 'b782b926aefe9fee1115ecfbb6d8f3bcf5b8981cbb90d89a'
@@ -15,13 +17,14 @@ num_classes = 3
 latent_dim =128
 list = ['anime', 'cartoon', 'human']
 
-print('333333333333333333333333333333333333333333')
+
 @app.route('/')
 def index():
     return render_template('submission.html')
 
 @app.route('/prediction', methods=('GET','POST'))    
 def gan():
+    session.clear()
     #Generate noise with input of label number
     class_num = int(request.form['LabelNumber'])
     label_name = list[class_num]
@@ -31,24 +34,34 @@ def gan():
             [random_latent_vectors, one_hot_labels], axis=1)
     #Load the model and generate image with noise
     gan_model = keras.models.load_model('cGAN.h5', compile=False)
-    print('--------------------------------------------')
-    image = gan_model.predict(random_vector_labels, verbose=0)
-    gan_model = None
-    img = (image * 127.5 + 127.5).astype(np.uint8)
+    image = gan_model(random_vector_labels)
+    img = (image * 127.5 + 127.5)
+    del gan_model, image
+    gc.collect()
 
     #Plot the image
     fig,ax = plt.subplots(1)
     fig.subplots_adjust(left=0,right=1,bottom=0,top=1)
-    ax.imshow(np.squeeze(img))
+    ax.imshow(np.squeeze(img).astype(np.uint8))
     ax.axis('tight')
     ax.axis('off')
     pngImage = BytesIO()
     FigureCanvas(fig).print_png(pngImage)
+    plt.close(fig)
 
     # Encode PNG image to base64 string
     pngImageB64String = "data:image/png;base64,"
     pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
-    return render_template('/prediction.html', image=pngImageB64String, label=label_name)
+    value = render_template('/prediction.html', image=pngImageB64String, label=label_name)
+    del fig, ax
+    del img
+    pngImage.flush()
+    pngImage.seek(0)
+    del pngImage
+    del pngImageB64String
+    gc.collect()
+    session.clear()
+    return value
 
 if(__name__ == "__main__"):
     app.run(debug=True)
